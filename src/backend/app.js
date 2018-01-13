29409b2ee42a1e27486a5319d041d0c479073fdc
mikko kotamies <mikko.kotamies@avarko.com>
2018-01-13 11:20:37 +0200

    Added template for post and get

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,21 +2,27 @@ const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
 // Connection URL
+console.log(process.env.PASSWD);
 const url =
-  "mongodb://admin:Roguelike2018!@roguelike-shard-00-00-ddtmn.mongodb.net:27017,roguelike-shard-00-01-ddtmn.mongodb.net:27017,roguelike-shard-00-02-ddtmn.mongodb.net:27017/test?ssl=true&replicaSet=roguelike-shard-0&authSource=admin";
+  "mongodb://admin:" +
+  process.env.PASSWD +
+  "@roguelike-shard-00-00-ddtmn.mongodb.net:27017,roguelike-shard-00-01-ddtmn.mongodb.net:27017,roguelike-shard-00-02-ddtmn.mongodb.net:27017/test?ssl=true&replicaSet=roguelike-shard-0&authSource=admin";
 
 // Database Name
 const dbName = "roguelike";
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const accessDB = callback =>
+  MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+    const db = client.db(dbName);
+    const collection = db.collection("games");
+    const result = callback(collection);
 
-  client.close();
-});
+    client.close();
+  });
 
 const express = require("express");
 const app = express();
@@ -26,9 +32,16 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
 
 app.post("/games", function(req, res) {
+  accessDB(collection => collection.insert({ test: "TESTI TEKSTI 2" }));
   res.send("TOIMII!");
 });
 
-app.post("/games", function(req, res) {
-  res.send("TOIMII!");
+app.get("/games", function(req, res) {
+  console.log("games");
+  accessDB(collection => {
+    collection.find().toArray((err, result) => {
+      if (err) console.log(err);
+      res.send(result);
+    });
+  });
 });
