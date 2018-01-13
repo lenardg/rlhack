9673a448f2c710aa264873b1e2d92f4e747a1564
Lenard Gunda <lenard@offbeat.fi>
2018-01-13 10:25:47 +0200

    Fix build script

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,11 @@
 @echo off
 echo "NPM"
+cd src\rl\
 call npm install
 echo "Generate static file"
 call npm run build
 echo "Deploy"
+cd ..\..
 xcopy %DEPLOYMENT_SOURCE%\src\rl\index.html %DEPLOYMENT_TARGET% /Y /E
 xcopy %DEPLOYMENT_SOURCE%\src\rl\app.bundle.js %DEPLOYMENT_TARGET% /Y /E
 xcopy %DEPLOYMENT_SOURCE%\dist\web.config %DEPLOYMENT_TARGET% /Y /E
